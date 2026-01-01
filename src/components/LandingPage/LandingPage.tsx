f975678027e953b20c12673ec2f07133e8841fc4
bul-bulBa <swyatoslav24042010@gmail.com>
2026-01-01 19:05:11 +0200

    start making themes

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -5,12 +5,15 @@ import Contacts from "./Contacts"
 import gugugaga from '../../assets/gugu-gagaScreen.png'
 import clearDev from '../../assets/clearDevScreen.png'
 import Slide from "./about-me/Slide"
+import Nav from './Nav'
 
 const LandingPage = () => {
     const page = useWheel(4)
 
     return (
         <div className="">
+            <Nav />
+
             {page === 1 && <AboutMe />}
             {page === 2 && <Project text="gugu gaga site" img={gugugaga} />}
             {page === 3 && <Project text="clearDevSite" img={clearDev} />}
